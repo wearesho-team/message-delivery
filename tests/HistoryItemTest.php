79b8ffac47f6414936b0f9b25cebaf24d60702cb
Alexander Letnikow <reclamme@gmail.com>
2018-05-12 16:41:14 +0300

    Test Delivery\HistoryItem

---
+++ tests/HistoryItemTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Wearesho\Delivery\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Wearesho\Delivery;
+
+/**
+ * Class HistoryItemTest
+ * @package Wearesho\Delivery\Tests
+ */
+class HistoryItemTest extends TestCase
+{
+    public function testCreate(): void
+    {
+        $message = new Delivery\Message(
+            "text",
+            "recipient"
+        );
+
+        $before = new \DateTime();
+        $item = new Delivery\HistoryItem($message, static::class, true);
+        $after = new \DateTime();
+
+        $this->assertEquals("text", $item->getText());
+        $this->assertEquals("recipient", $item->getRecipient());
+
+        $this->assertEquals(true, $item->isSent());
+        $this->assertEquals(static::class, $item->getSender());
+        $this->assertTrue(
+            $item->getSentAt() >= $before && $item->getSentAt() <= $after
+        );
+    }
+}
