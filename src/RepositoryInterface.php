6ce91d32db52d674bf37c16f69295a6d113e2d8b
Alexander Letnikow <reclamme@gmail.com>
2018-05-12 17:26:24 +0300

    Add getHistoryItem, save methods to Delivery\RepositoryInterface

--- src/RepositoryInterface.php
+++ src/RepositoryInterface.php
@@ -2,6 +2,8 @@
 
 namespace Wearesho\Delivery;
 
+use Wearesho\Delivery;
+
 /**
  * Interface RepositoryInterface
  * Storage for messages
@@ -14,29 +16,33 @@ namespace Wearesho\Delivery;
 interface RepositoryInterface
 {
     /**
-     * @param MessageInterface $message
+     * @param Delivery\MessageInterface $message
      * @param string $sender Sending channel <ServiceInterface>
      * @param bool $sent If channel correctly sent message
      */
-    public function push(MessageInterface $message, string $sender, bool $sent): void;
+    public function push(Delivery\MessageInterface $message, string $sender, bool $sent): void;
 
     /**
-     * @param MessageInterface $message
+     * @param Delivery\MessageInterface $message
      * @return bool|null
      *
      * MUST return null if message did not sent before
      * MUST return last sent status for message
      * If message sent few times, last status HAVE TO be returned
      */
-    public function isSent(MessageInterface $message): ?bool;
+    public function isSent(Delivery\MessageInterface $message): ?bool;
 
     /**
-     * @param MessageInterface $message
+     * @param Delivery\MessageInterface $message
      * @return null|string
      *
      * MUST return null if message did not sent before
      * MUST return sender if message sent before
      * if message sent few times last sender HAVE TO be returned
      */
-    public function getSender(MessageInterface $message): ?string;
+    public function getSender(Delivery\MessageInterface $message): ?string;
+
+    public function getHistoryItem(Delivery\MessageInterface $message): ?Delivery\HistoryItemInterface;
+
+    public function save(Delivery\HistoryItemInterface $item): void;
 }
